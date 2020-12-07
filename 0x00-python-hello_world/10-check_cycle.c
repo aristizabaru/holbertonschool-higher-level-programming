eab6b04cf78c22ddadc7f0dfe60e1bd6d26ba7c8
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-07 18:37:00 +0000

    REfactor(early detection)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -17,7 +17,7 @@ int check_cycle(listint_t *list)
     hare = list->next->next;
     turtoise = list->next;
 
-    while (turtoise && hare && hare->next)
+    while (hare && hare->next)
     {
         if (hare == turtoise)
         {
