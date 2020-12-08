efb4c4b5454d7f4331041ccec989bf813966c25f
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-08 22:36:58 +0000

    Refactor(print): refactor how to print the statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-signo = ""
 if number < 0:
-    signo = "-"
-if abs(number) % 10 is 0:
-    print("Last digit of {} is {} {}".format(
-        number,  abs(number) % 10, "and is zero"))
-elif abs(number) % 10 > 5 and number > 0:
-    print("Last digit of {} is {}{} {}".format(
-        number, signo, abs(number) % 10, "and is greater than 5"))
+    remainder = number % -10
 else:
-    print("Last digit of {} is {}{} {}".format(
-        number, signo, abs(number) % 10, "and is less than 6 and not 0"))
+    remainder = number % 10
+print('Last digit of', number, 'is', remainder, end=' ')
+if remainder > 5:
+    print('and is greater than 5')
+elif remainder == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
