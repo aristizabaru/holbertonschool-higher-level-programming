b319ac1ba43f672c2cc9b13a04cdce143c4179d2
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-15 19:38:20 +0000

    Refactor(print_matrix)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for idx, element in enumerate(row):
-                if idx is not len(row)-1:
-                    print("{:d} ".format(element), end="")
-                else:
-                    print("{:d}".format(element))
+                print("{:d}".format(element), end="")
+                if idx is not len(row) - 1:
+                    print(" ", end="")
+            print("")
