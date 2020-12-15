3693718ff05757a1b064b8b5864eca6cd3d30e54
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-15 18:26:49 +0000

    Test(matrix present): test if there is a matrix to print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for idx, element in enumerate(row):
-            if idx is not len(row)-1:
-                print("{:d} ".format(element), end="")
-            else:
-                print("{:d}".format(element))
+    if matrix:
+        for row in matrix:
+            for idx, element in enumerate(row):
+                if idx is not len(row)-1:
+                    print("{:d} ".format(element), end="")
+                else:
+                    print("{:d}".format(element))
