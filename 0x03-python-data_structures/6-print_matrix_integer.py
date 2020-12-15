a9358d58b46496ed3dbfb93903037767374e8b1a
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-15 18:24:29 +0000

    Style(print format): add :d for intergers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx, element in enumerate(row):
             if idx is not len(row)-1:
-                print("{} ".format(element), end="")
+                print("{:d} ".format(element), end="")
             else:
-                print("{}".format(element))
+                print("{:d}".format(element))
