3e29ea4bd6203de996400e31a6ecfb61a44662fa
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-16 04:45:02 +0000

    Feat(File Upload)

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+        int i = 0;
+        if (p)
+        {
+                printf("[*] Size of the Python List = %d\n", (int)Py_SIZE(p));
+                printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
+                for (i = 0; i < PyList_Size(p); i++)
+                {
+                        if (PyTuple_Check(PyList_GetItem(p, i)))
+                                printf("Element %d: tuple\n", i);
+                        else if (PyFloat_Check(PyList_GetItem(p, i)))
+                                printf("Element %d: float\n", i);
+                        else if (PyLong_Check(PyList_GetItem(p, i)))
+                                printf("Element %d: int\n", i);
+                        else if (PyUnicode_Check(PyList_GetItem(p, i)))
+                                printf("Element %d: str\n", i);
+                        else if (PyList_Check(PyList_GetItem(p, i)))
+                                printf("Element %d: list\n", i);
+                }
+        }
+}
