0115f3ca61d40830c2ded1d4698124c21793b2cf
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-16 03:14:39 +0000

    Docs(remove comments)

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,21 +3,6 @@
 
 int is_palindrome(listint_t **head)
 {
-
-        /**
-         * 1. Revisar si head existe -ok
-         * 2. Crear buffer -ok
-         * 3. Crear var length_buffer -ok
-         * 4. Recorrer linked -ok
-         * 4.1 Aumento length_buffer -ok
-         * 4.2 Paso dato de nodo->n a buffer[length_buffer] -ok
-         * 4.3 Cuando nodo->next == NULL cierro el buffer con \0 -ok
-         * 5. Recorro el buffer en las dos direcciones hasta
-         *    que left >= rigth
-         * 5.2 Comparo datos, si no son iguales NOT_PALINDROME
-         *     si son iguales IS_PALINDROME
-         */
-
         if (*head && (*head)->next)
         {
                 int buffer[1024 * 5];
