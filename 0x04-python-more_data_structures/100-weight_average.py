228a029d8ca2dc66cdbe83f88012a929c32a4171
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-17 21:34:40 +0000

    Test: check if there is a list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,10 @@ def weight_average(my_list=[]):
     result = 0
     mul = 0
     add = 0
-    for item in my_list:
-        temp = item[0] * item[1]
-        add += item[1]
-        mul += temp
-    result = mul / add
+    if my_list:
+        for item in my_list:
+            temp = item[0] * item[1]
+            add += item[1]
+            mul += temp
+        result = mul / add
     return result
