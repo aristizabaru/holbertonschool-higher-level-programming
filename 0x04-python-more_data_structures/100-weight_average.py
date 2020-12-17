25dfccf4e0d0378c0e46808cf87effc1f938124d
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-17 21:32:36 +0000

    Feat: File Update)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    result = 0
+    mul = 0
+    add = 0
+    for item in my_list:
+        temp = item[0] * item[1]
+        add += item[1]
+        mul += temp
+    result = mul / add
+    return result
