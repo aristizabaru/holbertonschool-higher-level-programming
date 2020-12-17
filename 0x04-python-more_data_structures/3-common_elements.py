66e04148d2fc789a9ba09825a5a18ba93710ef3a
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-17 20:31:23 +0000

    Test: checks if only one list is passed

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,9 @@ def common_elements(set_1, set_2):
     if set_1 and set_2:
         new_list = set_1 & set_2
         return new_list
+    elif set_1:
+        return set_1
+    elif set_2:
+        return set_2
     else:
         return None
