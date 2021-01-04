f7bb19c0c1c42b00dc6221f442d0a20f60f598c1
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-04 19:37:43 +0000

    Feat: add 100-safe_print_interger

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err))
+        return False
+    else:
+        return True
