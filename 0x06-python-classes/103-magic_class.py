d63eaa775d4fd3005d5579eccbe469f56d7eb5f7
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-06 02:33:07 +0000

    Refactor(__radius): change _MagicClass__radius for __radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,15 +8,15 @@ class MagicClass:
 
     def __init__(self, radius):
         """Initializes the Magic Class"""
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """Calculaes the area of the circle"""
-        return (self._MagicClass__radius ** 2) * math.pi
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """Calculates the circumference of the circle"""
-        return (2 * math.pi) * self._MagicClass__radius
+        return (2 * math.pi) * self.__radius
