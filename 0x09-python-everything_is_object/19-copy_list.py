39067ca54c47d71f5f6d2f4dafb071cbbdc16a1d
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-12 23:37:14 +0000

    Docs: Add docstring

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""19-copy_list module
+
+Functions:
+    copy_list(l)
+"""
+
+
 def copy_list(l):
+    """copy a list and return a new one
+
+    Arguments:
+        l (list): list to be duplicated
+
+    Return:
+        copy of `l``
+    """
     new_list = l[:]
     return new_list
