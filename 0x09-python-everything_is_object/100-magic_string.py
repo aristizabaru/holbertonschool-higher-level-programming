db6b57ac60b7461143cea98209eb712fca2fc2e2
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-13 00:14:25 +0000

    Docs: add docstring

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+""" module 100-magic_string
+"""
+
+
 def magic_string(a=[]):
+    """ return a srring
+    """
     a += ["Holberton"]
     return str(a)[1:-1]
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""module 101-locked_class
+"""
+
+
 class LockedClass:
+    """creates a LockedClass object
+    """
     __slots__ = ["first_name"]
