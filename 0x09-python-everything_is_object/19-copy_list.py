7e453a9ea0d1a039c7ebcb9b50e6fb0bb8307239
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-13 00:05:05 +0000

    Fix: erase docstring

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-"""19-copy_list module
-
-Functions:
-    copy_list(lis)
-"""
-
-
-def copy_list(lis):
-    """copy a list and return a new one
-
-    Arguments:
-        lis(list): list to be duplicated
-
-    Return:
-        copy of `lis`
-    """
-    new_list = lis[:]
-    return new_list
+def copy_list(l):
+    return l[:]
