249e018cd23d6830c0194168df20d639c3ce0d44
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-20 23:19:07 +0000

    Docs(PEP8): fix trailing white spacing in docstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,7 +7,7 @@ Functions:
 
 
 def append_write(filename="", text=""):
-    """wappends a string at the end of a text file (UTF8) 
+    """wappends a string at the end of a text file (UTF8)
     and returns the number of characters added
 
     Arguments:
