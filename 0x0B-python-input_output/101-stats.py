ca4c3f7d6420c3ceda9a5c05776762dc77b9345d
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-01-21 04:46:16 +0000

    Fix: status codes count

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,7 +14,7 @@ status_codes = dict()
 try:
     for data in sys.stdin:
         data = data.rstrip()
-        print(data)
+        # print(data)
         # find file size
         start_idx = data.rfind(" ") + 1
         file_size += int(data[start_idx:])
@@ -30,9 +30,10 @@ try:
             sort_codes = sorted(status_codes.items())
             for key, value in sort_codes:
                 print("{}: {}".format(key, value))
-            print("-"*50)
+            # print("-"*50)
         lines += 1
 except KeyboardInterrupt:
     print("File size: {}".format(file_size))
-    for key in status_codes:
-        print("{}: {}".format(key, status_codes[key]))
+    sort_codes = sorted(status_codes.items())
+    for key, value in sort_codes:
+        print("{}: {}".format(key, value))
