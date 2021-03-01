8eb4ed744c946af5b5e813d9830ed66cede748eb
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-03-01 21:36:47 +0000

    Fix: after ALTER database to utf8mb4_unicode_ci character set add USE hbtn_0c_0 to change tables

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 -- in your MySQL server.
 ALTER DATABASE databasename CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE tablename CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
