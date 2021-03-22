b0e902ba390962b12931a060c67f3a8f76f9d6dc
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-03-22 22:52:47 +0000

    Style: fix indentation

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-function add(a, b) {
-   console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]))
+function add (a, b) {
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
 }
 add();
