c66aa5efa0314a180399ed762055e66e05fe1137
aristizabaru <andres.aristizabal@gmail.com>
2021-04-08 19:08:09 +0000

    Fix: fix Origin header

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl 0.0.0.0:5000/catch_me -sL -X PUT -d "user_id=98" -H "origin: HolbertonSchool"
+# Makes loking for a respond with a message containing You got me!
+curl 0.0.0.0:5000/catch_me -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
+
+
+#!/bin/bash
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
