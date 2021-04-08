5e9d88bc6c77436dbfc4d1d77f462a458fce03ff
aristizabaru <andres.aristizabal@gmail.com>
2021-04-08 19:04:29 +0000

    Fix: change in wich json object is transfered

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL passed as the first argument
-curl -s "$1" -X POST -H "Content-type: application/json" -T "$2"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
